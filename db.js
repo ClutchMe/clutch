5c0491426986816f43ee727bd1120aa55068d962
ClutchMe <hamada_9991@outlook.com>
2025-06-24 12:18:18 +0000

    ..

--- db.js
+++ db.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ MongoDB connected'))
-.catch((err) => console.error('❌ MongoDB connection error:', err));
+// db.js content here
